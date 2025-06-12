5497854eb5896d1c0f597d863b62be005e2b81ad
Wahyudin <muhammadwahyudin0701@gmail.com>
2025-06-12 11:55:39 +0700

    Memberbaiki Struktur atribut di Class User

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'last_name', 'email', 'password',
+        'name',
+        'last_name',
+        'email',
+        'password',
     ];
 
     /**
